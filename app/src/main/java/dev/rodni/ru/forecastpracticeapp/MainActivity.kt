d3cfd81b4fe9f04e5bc7df070ffc7f45a9964840
rodion <rodion.prosvirnin@bk.ru>
2019-06-25 13:36:34 +0300

    ui, resources and navigation work

--- app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
+++ app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
@@ -2,11 +2,35 @@ package dev.rodni.ru.forecastpracticeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    //note in the bottom
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 }
+
+/*
+Иногда переменную нельзя сразу инициализировать, сделать это можно чуть позже.
+Для таких случаев придумали новый модификатор lateinit (отложенная инициализация).
+
+
+private lateinit var button: Button
+Переменная обязательно должна быть изменяемой (var).
+Не должна относиться к примитивным типам (Int, Double, Float и т.д).
+Не должна иметь собственных геттеров/сеттеров.
+
+Подобный подход удобен во многих случаях, избегая проверки на null.
+В противном случае пришлось бы постоянно использовать проверку или утверждение !!, что засоряет код.
+
+Если вы обратитесь к переменной до её инициализации,
+то получите исключение "lateinit property ... hos not been initialized" вместо NullPointerException.
+ */

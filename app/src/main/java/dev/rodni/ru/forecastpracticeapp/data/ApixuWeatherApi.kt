eb8815cb74491a536a98771b2c9988f1eff4f990
rodion <rodion.prosvirnin@bk.ru>
2019-07-10 10:39:51 +0300

    entities creation the first step

---
+++ app/src/main/java/dev/rodni/ru/forecastpracticeapp/data/ApixuWeatherApi.kt
@@ -0,0 +1,54 @@
+package dev.rodni.ru.forecastpracticeapp.data
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import dev.rodni.ru.forecastpracticeapp.data.response.CurrentWeatherResponse
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY = "ce87d753c17b4b31bc164326191007"
+
+//http://api.apixu.com/v1/current.json?key=ce87d753c17b4b31bc164326191007&q=Kazan&lang=ru
+
+interface ApixuWeatherApi {
+
+    @GET("current.json")
+    fun getCurrentWeather(
+        @Query("q") location: String,
+        @Query("lang") languageCode: String = "ru"
+    ) : Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke() : ApixuWeatherApi {
+            val requestInterceptor = Interceptor { chain ->  
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://api.apixu.com/v1/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApixuWeatherApi::class.java)
+        }
+    }
+}

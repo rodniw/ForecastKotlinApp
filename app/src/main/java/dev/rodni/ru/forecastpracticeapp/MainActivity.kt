09132400129cc6ba5e337bc907dd1b5ce5e1b3e6
rodion <rodion.prosvirnin@bk.ru>
2019-07-19 18:21:43 +0300

    fixed toolbar things

--- app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
+++ app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
 

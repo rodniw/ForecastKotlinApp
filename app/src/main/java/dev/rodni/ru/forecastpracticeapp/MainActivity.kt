65416577601d46a7ec11cadcf6de5034de6d1c6f
rodion <rodion.prosvirnin@bk.ru>
2019-07-23 19:37:43 +0300

    runtime permissions work done

--- app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
+++ app/src/main/java/dev/rodni/ru/forecastpracticeapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package dev.rodni.ru.forecastpracticeapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -9,10 +14,13 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
+import dev.rodni.ru.forecastpracticeapp.ui.LifecycleBoundLocationManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
+
+private const val MY_PERMISSION_ACCESS_COARSE_LOCATION = 1
 
 class MainActivity : AppCompatActivity(), KodeinAware {
 
@@ -39,12 +47,55 @@ class MainActivity : AppCompatActivity(), KodeinAware {
 
         NavigationUI.setupActionBarWithNavController(this, navController)
 
+        requestLocationPermission()
 
-
+        if (hasLocationPermission()) {
+            bindLocationManager()
+        }
+        else
+            requestLocationPermission()
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(null, navController)
+        return NavigationUI.navigateUp(navController, null)
+    }
+
+    private fun bindLocationManager() {
+        LifecycleBoundLocationManager(
+            this,
+            fusedLocationProviderClient, locationCallback
+        )
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSION_ACCESS_COARSE_LOCATION
+        )
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == MY_PERMISSION_ACCESS_COARSE_LOCATION) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                bindLocationManager()
+            else
+                Toast.makeText(
+                    this,
+                    getString(R.string.set_loc_permission_manually),
+                    Toast.LENGTH_LONG
+                ).show()
+        }
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
 
